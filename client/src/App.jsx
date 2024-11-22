c45f06949abca74375ff94fe508c02f2d601f712
Nimira43 <n.rabone72@aol.com>
2024-11-22 23:58:34 +0000

    Routing to Dashboard working fine.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom' 
 import Login from './dashboard/pages/Login'
+import MainLayout from './dashboard/layout/MainLayout'
 function App() {
   return (
     <div>
@@ -7,6 +8,9 @@ function App() {
       <Routes>
         <Route path='/login' element={<Login /> }/>
       </Routes>
+      <Route path='/dashboard' element={<MainLayout />}>
+
+      </Route>
       </BrowserRouter> 
     </div>
   )
