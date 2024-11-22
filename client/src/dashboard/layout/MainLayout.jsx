27f9e369e353efa7052e556760d541426cd80024
Nimira43 <n.rabone72@aol.com>
2024-11-22 23:29:04 +0000

    Setup files and folders

---
+++ client/src/dashboard/layout/MainLayout.jsx
@@ -0,0 +1,9 @@
+const MainLayout = () => {
+  return (
+    <div>
+      <h1>MainLayout Page</h1>
+    </div>
+  )
+}
+
+export default MainLayout
---
+++ client/src/dashboard/layout/Sidebar.jsx
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+  return (
+    <div>
+      SideBar Page
+    </div>
+  )
+}
+
+export default Sidebar
