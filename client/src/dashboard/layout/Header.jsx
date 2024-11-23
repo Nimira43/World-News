0ded7826773ff890205014b0cd2dc9f49c1571aa
Nimira43 <n.rabone72@aol.com>
2024-11-23 04:07:42 +0000

    Brought in a new soft teal colour which becomes the primary color

--- client/src/dashboard/layout/Header.jsx
+++ client/src/dashboard/layout/Header.jsx
@@ -4,11 +4,11 @@ const Header = () => {
   return (
     <div className='pl-4 fixed w-[calc(100vw-250px)] top-4 z-50'>
       <div className='w-full rounded h-[70px] flex justify-between items-center p-4 bg-[#fff9f5]'>
-        <input type='text' placeholder='Search' className='px-3 py-2 rounded-md outline-0 border border-[#888] focus:border-[#333] h-10' />
+        <input type='text' placeholder='Search' className='px-3 py-2 rounded-md outline-0 bg-[#fff9f5] border border-[#888] focus:border-[#333] h-10' />
         <div className='mr-4'>
           <div className='flex gap-x-2'>
             <div className='flex flex-col justify-center items-end'>
-              <span className='font-semibold'>John Smith</span>
+              <span className='font-semibold text-[#4a9f9f]'>John Smith</span>
               <span>Administrator</span>
             </div>
             <img className='10 h-10 rounded-full border-2 border-[#888]' src={User} alt='User' />
--- client/src/dashboard/layout/Sidebar.jsx
+++ client/src/dashboard/layout/Sidebar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
-    <div className='w-[250px] h-screen fixed left-0 top-0 bg-[#888]'>
+    <div className='w-[250px] h-screen fixed left-0 top-0 bg-[#4a9f9f]'>
       <div className='h-[70px] flex justify-center items-center'>
         <Link to='/'>
           <span className='logo w-[190px] h-[35px]'>World News</span>
