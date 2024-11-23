468cdb7d7c1cb701787ba652e98c5cabba12e643
Nimira43 <n.rabone72@aol.com>
2024-11-23 00:18:43 +0000

    Setting out initial classes for Sidebar and Header in MainLayout

--- client/src/dashboard/layout/MainLayout.jsx
+++ client/src/dashboard/layout/MainLayout.jsx
@@ -1,14 +1,19 @@
 import Sidebar from './Sidebar'
 import Header from './Header'
+import { Outlet } from 'react-router-dom'
 
 const MainLayout = () => {
   return (
-    <div className>
+    <div className='min-w-screen min-h-screen bg-[#fafafa'>
       <Sidebar />
       <div>
         <Header />
+        <div className='p-4'>
+          <div className='pt-[85px]'>
+            <Outlet />
+          </div>
+        </div>
       </div>
-      <h1>MainLayout Page</h1>
     </div>
   )
 }
