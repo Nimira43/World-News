9117b4d5f9452d0ba580aa14bd764fd284682a39
Nimira43 <n.rabone72@aol.com>
2024-11-23 03:55:42 +0000

    Working on the sidebar and the logo. Making things simple. Refining the colours.

--- client/src/dashboard/layout/Header.jsx
+++ client/src/dashboard/layout/Header.jsx
@@ -2,16 +2,16 @@ import User from '../../assets/user.png'
 
 const Header = () => {
   return (
-    <div className='pl-4 fixed w-[calc(100vw-250px)] top-4 z-500'>
-      <div className="w-full rounded h-[70px] flex justify-between items-center p-4 bg-[#fff9f5]">
+    <div className='pl-4 fixed w-[calc(100vw-250px)] top-4 z-50'>
+      <div className='w-full rounded h-[70px] flex justify-between items-center p-4 bg-[#fff9f5]'>
         <input type='text' placeholder='Search' className='px-3 py-2 rounded-md outline-0 border border-[#888] focus:border-[#333] h-10' />
-        <div className="mr-4">
-          <div className="flex gap-x-2">
-            <div className="flex flex-col justify-center items-end">
+        <div className='mr-4'>
+          <div className='flex gap-x-2'>
+            <div className='flex flex-col justify-center items-end'>
               <span className='font-semibold'>John Smith</span>
               <span>Administrator</span>
             </div>
-            <img className='10 h-10 rounded-full border-2 border-[#888]' src={User} alt="User" />
+            <img className='10 h-10 rounded-full border-2 border-[#888]' src={User} alt='User' />
           </div>
         </div>
       </div>
--- client/src/dashboard/layout/Sidebar.jsx
+++ client/src/dashboard/layout/Sidebar.jsx
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom'
+
 const Sidebar = () => {
   return (
-    <div className='w[250px] h-screen fixed left-0 top-0 bg-[#888]'>
-      
+    <div className='w-[250px] h-screen fixed left-0 top-0 bg-[#888]'>
+      <div className='h-[70px] flex justify-center items-center'>
+        <Link to='/'>
+          <span className='logo w-[190px] h-[35px]'>World News</span>
+        </Link>
+      </div>
     </div>
   )
 }
