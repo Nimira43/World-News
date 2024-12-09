72283ae38748b7a53824f552c773676d56e798a5
Nimira43 <n.rabone72@aol.com>
2024-12-09 11:40:19 +0000

    Brought in useLocation from React Router-DOM

--- client/src/dashboard/layout/Sidebar.jsx
+++ client/src/dashboard/layout/Sidebar.jsx
@@ -1,6 +1,8 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const Sidebar = () => {
+  const { pathname } = useLocation()
+
   return (
     <div className='w-[250px] h-screen fixed left-0 top-0 bg-[#4a9f9f]'>
       <div className='h-[70px] flex justify-center items-center'>
@@ -8,6 +10,12 @@ const Sidebar = () => {
           <span className='logo w-[190px] h-[35px]'>World News</span>
         </Link>
       </div>
+      <ul className="px-3 flex flex-col gap-y-1 font-medium">
+        <li>
+          <Link to='/dashboard/admin' className={`px-3 ${pathname}`}>
+          </Link>
+        </li>
+      </ul>
     </div>
   )
 }
