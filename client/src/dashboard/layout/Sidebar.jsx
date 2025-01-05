384afa3d05ca04dfcd46ae7db237089401dc66bb
Nimira43 <n.rabone72@aol.com>
2025-01-05 08:47:46 +0000

    Improved styling.

--- client/src/dashboard/layout/Sidebar.jsx
+++ client/src/dashboard/layout/Sidebar.jsx
@@ -4,7 +4,7 @@ const Sidebar = () => {
   const { pathname } = useLocation()
 
   return (
-    <div className='w-[250px] h-screen fixed left-0 top-0 bg-[#4a9f9f]'>
+    <div className='w-[250px] h-screen fixed left-0 top-0 bg-[#ededed]'>
       <div className='h-[70px] flex justify-center items-center'>
         <Link to='/'>
           <span className='logo w-[190px] h-[35px]'>World News</span>
