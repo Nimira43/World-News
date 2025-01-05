0bbd0906f894acc0ff1df95a9298d6dc58aa0e1b
Nimira43 <n.rabone72@aol.com>
2025-01-05 09:20:28 +0000

    Restyled logo. Brought new fonts in. Starting to work on the sidebar

--- client/src/dashboard/layout/Sidebar.jsx
+++ client/src/dashboard/layout/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { Link, useLocation } from 'react-router-dom'
+import { LuLayoutDashboard } from 'react-icons/lu'
 
 const Sidebar = () => {
   const { pathname } = useLocation()
@@ -12,7 +13,16 @@ const Sidebar = () => {
       </div>
       <ul className="px-3 flex flex-col gap-y-1 font-medium">
         <li>
-          <Link to='/dashboard/admin' className={`px-3 ${pathname}`}>
+          <Link 
+            to='/dashboard/admin' 
+            className={`
+              px-3 ${pathname === '/dashboard/admin' ?
+              'bg-[#333] text-[#fffaf0]' :
+              'bg-[#fffaf0] text-[#333]'} py-2 hover:shadow-lg hover:shadow-red-700/20 w-full rounded-lg flex gap-x-2 justify-start items-center hover:bg-[#ff4500] hover:text-[#fffaf0] 
+            `}
+          >
+            <span><LuLayoutDashboard /></span>
+            <span>Dashboard</span>
           </Link>
         </li>
       </ul>
