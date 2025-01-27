8d6306419ac1ebb69a9c36beaeff56f9a5f3e4b1
Nimira43 <n.rabone72@aol.com>
2025-01-27 00:55:46 +0000

    ProtectDashboard component ensures that only users with the userInfo object will have access to the nested routes

--- client/src/middleware/ProtectDashboard.jsx
+++ client/src/middleware/ProtectDashboard.jsx
@@ -1,9 +1,16 @@
+import { Navigate, Outlet } from 'react-router-dom'
+
 const ProtectDashboard = () => {
-  return (
-    <div>
-      Protect Dashboard
-    </div>
-  )
+  const userInfo = {
+    name: 'User',
+    role: 'admin'
+  }
+
+  if (userInfo) {
+    return <Outlet/>
+  } else {
+    return <Navigate to='/login' />
+  }
 }
 
 export default ProtectDashboard
