8ec70221b35141b804581cf0b17d0a5dec0e8277
Nimira43 <n.rabone72@aol.com>
2025-03-02 04:51:44 +0000

    Brought in ProtectedRole route for admin role.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,22 +3,41 @@ import Login from './dashboard/pages/Login'
 import MainLayout from './dashboard/layout/MainLayout'
 import AdminIndex from './dashboard/pages/AdminIndex'
 import ProtectDashboard from './middleware/ProtectDashboard'
+import ProtectRole from './middleware/ProtectRole'
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={<Login/>} />
-        <Route path='/dashboard' element={<ProtectDashboard/>} >
-          <Route path='' element={<MainLayout/>} >
-            <Route path='' element={<Navigate to='/dashboard/admin'/>} />
-            <Route path='admin' element={<AdminIndex />} />
+        <Routes>
+          <Route
+            path='/login'
+            element={<Login />}
+          />
+          <Route
+            path='/dashboard'
+            element={<ProtectDashboard />}
+          >
+            <Route
+              path=''
+              element={<MainLayout />}
+            >
+              <Route
+                path=''
+                element={<Navigate to='/dashboard/admin' />}
+              />
+              <Route
+                path=''
+                element={<ProtectRole role='admin' />}
+              >
+                <Route
+                  path='admin'
+                  element={<AdminIndex />}
+                />
+              </Route>     
+            </Route>
           </Route>
-          
-        </Route>
-      </Routes>
-      
+        </Routes>
       </BrowserRouter> 
     </div>
   )
