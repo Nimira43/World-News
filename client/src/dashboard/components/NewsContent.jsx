bc632e66cc3fa832f626884584da7e8e506f8ce4
Nimira43 <n.rabone72@aol.com>
2025-04-02 01:58:36 +0100

    Setting up NewContent container.

--- client/src/dashboard/components/NewsContent.jsx
+++ client/src/dashboard/components/NewsContent.jsx
@@ -1,7 +1,9 @@
 const NewsContent = () => {
   return (
-    <div>
-      News Content
+    <div className='bg-light min-h-screen p-6'>
+      <div className='flex items-center gap-4 mb-6'>
+        
+      </div>
     </div>
   )
 }
