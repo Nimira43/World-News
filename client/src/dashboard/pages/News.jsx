3d5e077aca81cb88c5e84fe882e8e62ffdead3f4
Nimira43 <n.rabone72@aol.com>
2025-04-02 01:31:18 +0100

    Starting to develop the News page.

--- client/src/dashboard/pages/News.jsx
+++ client/src/dashboard/pages/News.jsx
@@ -1,7 +1,9 @@
 const News = () => {
   return (
-    <div>
-      News Page
+    <div className='bg-grey-light rounded-md'>
+      <div className='flex justify-between p-4'>
+        <h2 className='text-xl font-medium'>News</h2>
+      </div>
     </div>
   )
 }
