b89dd86d31c1a2c705c71d558d894a485c008b44
Nimira43 <n.rabone72@aol.com>
2025-04-02 02:29:00 +0100

    Table headings

--- client/src/dashboard/components/NewsContent.jsx
+++ client/src/dashboard/components/NewsContent.jsx
@@ -10,6 +10,23 @@ const NewsContent = () => {
         </select>
         <input type='text' placeholder='Search' className='w-full px-4 py-4 rounded-md border border-grey-medium outline-0 focus:border-grey-dark h-10' />
       </div>
+      <div className='overflow-x-auto'>
+        <table className='w-full table-auto bg-light shadow-lg rounded-lg overflow-hidden'>
+          <thead className='bg-grey-light text-primary uppercase text-sm'>
+            <tr>
+              <th className='py-4 px-6 text-left'>No</th>
+              <th className='py-4 px-6 text-left'>Title</th>
+              <th className='py-4 px-6 text-left'>Image</th>
+              <th className='py-4 px-6 text-left'>Category</th>
+              <th className='py-4 px-6 text-left'>Description</th>
+              <th className='py-4 px-6 text-left'>Date</th>
+              <th className='py-4 px-6 text-left'>Status</th>
+              <th className='py-4 px-6 text-left'>Action</th>
+            </tr>
+          </thead>
+
+        </table>
+      </div>
     </div>
   )
 }
