cf78656beb86be418bf266da0a40ab06f2d5c709
Nimira43 <n.rabone72@aol.com>
2025-04-02 01:49:33 +0100

    Link to Create News Story

--- client/src/dashboard/pages/News.jsx
+++ client/src/dashboard/pages/News.jsx
@@ -1,8 +1,24 @@
+import { Link } from 'react-router-dom'
+
 const News = () => {
+
+  const userInfo = {
+    role: 'admin'
+  }
+
   return (
     <div className='bg-grey-light rounded-md'>
       <div className='flex justify-between p-4'>
         <h2 className='text-xl font-medium'>News</h2>
+        {
+          userInfo.role !== 'admin' &&
+            <Link
+              className='px-4 py-[8px] bg-grey-dark rounded-lg text-light hover:bg-primary'
+              to='/dashboard/news/create' 
+            >
+              Create News Story
+            </Link>
+        }
       </div>
     </div>
   )
