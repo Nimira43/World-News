a15786d76f55409a2bf679798b669776d346d968
Nimira43 <n.rabone72@aol.com>
2025-04-02 02:05:24 +0100

    Select Status select menu

--- client/src/dashboard/components/NewsContent.jsx
+++ client/src/dashboard/components/NewsContent.jsx
@@ -2,7 +2,12 @@ const NewsContent = () => {
   return (
     <div className='bg-light min-h-screen p-6'>
       <div className='flex items-center gap-4 mb-6'>
-        
+        <select name='status' className='w-48 px-4 py-2 rounded-md border border-grey-dark'>
+          <option value=''>Select Status</option>
+          <option value='pending'>Pending</option>
+          <option value='active'>Active</option>
+          <option value='deactive'>Deactive</option>
+        </select>
       </div>
     </div>
   )
