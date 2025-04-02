a65a460dcbd3050e913618dcb32e0762f3d2ab52
Nimira43 <n.rabone72@aol.com>
2025-04-02 02:17:38 +0100

    Search bar in NewsContent styled.

--- client/src/dashboard/components/NewsContent.jsx
+++ client/src/dashboard/components/NewsContent.jsx
@@ -2,12 +2,13 @@ const NewsContent = () => {
   return (
     <div className='bg-light min-h-screen p-6'>
       <div className='flex items-center gap-4 mb-6'>
-        <select name='status' className='w-48 px-4 py-2 rounded-md border border-grey-dark'>
+        <select name='status' className='w-48 px-4 py-2 rounded-md border border-grey-medium outline-0 focus:border-grey-dark h-10'>
           <option value=''>Select Status</option>
           <option value='pending'>Pending</option>
           <option value='active'>Active</option>
           <option value='deactive'>Deactive</option>
         </select>
+        <input type='text' placeholder='Search' className='w-full px-4 py-4 rounded-md border border-grey-medium outline-0 focus:border-grey-dark h-10' />
       </div>
     </div>
   )
--- client/src/dashboard/pages/News.jsx
+++ client/src/dashboard/pages/News.jsx
@@ -8,7 +8,7 @@ const News = () => {
   }
 
   return (
-    <div className='bg-grey-light rounded-md'>
+    <div className='bg-light rounded-md'>
       <div className='flex justify-between p-4'>
         <h2 className='text-xl font-medium'>News</h2>
         {
