44b3069ee200bf4c09a17b7b9431e44639000a17
Nimira43 <n.rabone72@aol.com>
2025-05-23 22:41:05 +0100

    Added new theme colours to tailwind.config.

--- client/src/dashboard/pages/AdminIndex.jsx
+++ client/src/dashboard/pages/AdminIndex.jsx
@@ -2,7 +2,9 @@ const AdminIndex = () => {
   return (
     <div className='mt-6'>
       <div className='grid grid-col-1 md:grid-cols-2 lg:grid-cols-5 gap-6'>
-        
+        {[
+          {title: 'World News', value: 173, color: 'text-blue-500'}
+        ]}
       </div>
       
     </div>
