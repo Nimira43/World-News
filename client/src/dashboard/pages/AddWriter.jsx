b62c1947cb745c23ee87ed34e5f9fb23022819ec
Nimira43 <n.rabone72@aol.com>
2025-07-03 04:27:19 +0100

    Added Link to Journalists page

--- client/src/dashboard/pages/AddWriter.jsx
+++ client/src/dashboard/pages/AddWriter.jsx
@@ -5,8 +5,15 @@ const AddWriter = () => {
     <div className='bg-light rounded-md'>
       <div className='flex justify-between p-4'>
         <h2 className='text-xl font-medium uppercase'> Add Journalist</h2>
-        <Link className='text-grey-dark hover:text-primary font-medium uppercase'></Link>
+        <Link 
+          to='/dashboard/writers'
+          className='text-grey-dark hover:text-primary font-medium uppercase'
+        >
+          Journalists
+        </Link>
       </div>
+
+      
     </div>
   )
 }
