18a2bc5592d3bd02059187b7f48690195cf75fa8
Nimira43 <n.rabone72@aol.com>
2025-07-03 05:13:50 +0100

    Added Category Select options

--- client/src/dashboard/pages/AddWriter.jsx
+++ client/src/dashboard/pages/AddWriter.jsx
@@ -15,19 +15,46 @@ const AddWriter = () => {
 
       <div className='p-4'>
         <form>
-          <div className=' grid grid-cols-2 gap-x-8 mb-3'>
-            <label 
-              htmlFor='name' 
-              className='text-grey-dark uppercase text-md'
-            >
-              Name  
-            </label>
-            <input 
-              type='text' 
-              name='name'
-              required
-              className='px3 py-2 rounded-md'
-            />
+          <div className='grid grid-cols-2 gap-x-8 mb-3'>
+            <div className='flex flex-col gap-y-2'>
+              <label 
+                htmlFor='name' 
+                className='text-grey-dark uppercase text-md'
+              >
+                Name  
+              </label>
+              <input
+                id='name' 
+                type='text' 
+                name='name'
+                required
+                className='px-3 py-2 rounded-md border border-grey-medium outline-0 focus:border-grey-dark h-10' 
+              />
+            </div>
+            <div className='flex flex-col gap-y-2'>
+              <label 
+                htmlFor='name' 
+                className='text-grey-dark uppercase text-md'
+              >
+                Category  
+              </label>
+              <select
+                id='name' 
+                name='category'
+                required 
+                className='w-48 px-3 py-2 rounded-md border border-grey-medium outline-0 focus:border-grey-dark h-10 text-left'
+              >
+                <option 
+                value=''>Select Status</option>
+                <option value='pending'>International</option>
+                <option value='active'>Business</option>
+                <option value='deactive'>Health</option>
+                <option value='deactive'>Entertainment</option>
+                <option value='deactive'>Sport</option>
+                <option value='deactive'>Technology</option>
+                <option value='deactive'>Travel</option>
+              </select>
+            </div>
           </div>
         </form>
       </div>
