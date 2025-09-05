d6431a232e0ad5db758b610118c5ba953d0f9193
Nimira43 <n.rabone72@aol.com>
2025-09-05 11:16:12 +0100

    Creating Label with icon

--- client/src/dashboard/pages/Profile.jsx
+++ client/src/dashboard/pages/Profile.jsx
@@ -1,8 +1,17 @@
+import { RiImageCircleLine } from 'react-icons/ri'
+
 const Profile = () => {
   return (
     <div className='w-full grid grid-cols-1 lg:grid-cols-2 gap-2 mt-5'>
-      <div className=''>
-        
+      <div className='p-6 flex items-center'>
+        <div className='flex-shrink-0'>
+          <label 
+            htmlFor='img'
+            className='w-[150px] h-[150px] flex flex-col justify-center items-center rounded-full bg-grey-light-extra border-2 border-grey-medium text-grey-dark cursor-pointer hover:bg-primary label'
+          >
+            <RiImageCircleLine />
+          </label>
+        </div>
       </div>
     </div>
   )
