46949b72287b2a0b6673a6096a4e011a48a7aeff
Nimira43 <n.rabone72@aol.com>
2025-09-05 11:20:05 +0100

    Updated label and icon

--- client/src/dashboard/pages/Profile.jsx
+++ client/src/dashboard/pages/Profile.jsx
@@ -3,13 +3,15 @@ import { RiImageCircleLine } from 'react-icons/ri'
 const Profile = () => {
   return (
     <div className='w-full grid grid-cols-1 lg:grid-cols-2 gap-2 mt-5'>
-      <div className='p-6 flex items-center'>
+      <div className='bg-grey-light-extra p-6 flex items-center'>
         <div className='flex-shrink-0'>
           <label 
             htmlFor='img'
-            className='w-[150px] h-[150px] flex flex-col justify-center items-center rounded-full bg-grey-light-extra border-2 border-grey-medium text-grey-dark cursor-pointer hover:bg-primary label'
+            className='w-[150px] h-[150px] flex flex-col justify-center items-center rounded-full bg-light border-2 border-grey-medium text-grey-dark cursor-pointer hover:bg-primary hover:text-light label'
           >
-            <RiImageCircleLine />
+            <RiImageCircleLine 
+              className='text-4xl'
+            />
           </label>
         </div>
       </div>
