a5c49487867438b7642b6552f42c32594dac6226
Nimira43 <n.rabone72@aol.com>
2025-11-09 14:48:11 +0000

    App - routes refactor - 1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const userInfo = {
     role: 'writer'
   }
-
+  <Navigate to='/dashboard/admin' />
   return (
     <BrowserRouter>
       <Routes>
@@ -24,7 +24,7 @@ function App() {
         <Route path='/dashboard' element={<ProtectDashboard />} >
           <Route path='' element={<MainLayout />} >
             
-            <Route path='' element={<Navigate to='/dashboard/admin' />} />
+            <Route path='' element={userInfo} />
             <Route path='unable-access' element={<Unable />} />
             
             <Route path='profile' element={<Profile />} />
