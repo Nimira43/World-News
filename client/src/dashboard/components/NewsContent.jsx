67854b2e8f3b6738547bf8064ee54f0f22c3ba86
Nimira43 <n.rabone72@aol.com>
2025-11-26 14:10:38 +0000

    selector and search-bar

--- client/src/dashboard/components/NewsContent.jsx
+++ client/src/dashboard/components/NewsContent.jsx
@@ -9,13 +9,19 @@ const NewsContent = () => {
   return (
     <div className='bg-light min-h-screen p-6'>
       <div className='flex items-center gap-4 mb-6'>
-        <select name='status' className='w-48 px-4 py-2 rounded-md border border-grey-medium outline-0 focus:border-grey-dark h-10'>
+        <select 
+          name='status' 
+          className='selector'
+        >
           <option value=''>Select Status</option>
           <option value='pending'>Pending</option>
           <option value='active'>Active</option>
           <option value='deactive'>Deactive</option>
         </select>
-        <input type='text' placeholder='Search' className='w-full px-4 py-4 rounded-md border border-grey-medium outline-0 focus:border-grey-dark h-10' />
+        <input 
+          type='text' 
+          placeholder='Search' className='search-bar' 
+        />
       </div>
       <div className='overflow-x-auto'>
         <table className='w-full table-auto bg-grey-light-extra shadow-lg rounded-md overflow-hidden'>
