a6b9818afa77e4066a185957c30332bef4c45ec9
Nimira43 <n.rabone72@aol.com>
2025-12-12 10:16:07 +0000

    CreateNews - 3

--- client/src/dashboard/pages/CreateNews.jsx
+++ client/src/dashboard/pages/CreateNews.jsx
@@ -5,7 +5,9 @@ const CreateNews = () => {
     <div>
       <div className='just-between mb-6 p-4'>
         <h2 className='text-xl font-medium uppercase'>Add News</h2>
-        <Link className='hover:text-primary font-medium uppercase'>
+        <Link
+          to='/dashboard/news'
+          className='hover:text-primary font-medium uppercase'>
           View All
         </Link>
       </div>
