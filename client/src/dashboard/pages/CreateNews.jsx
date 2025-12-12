fb2a21c4a478fbc8cdf8c2576492c4f157666859
Nimira43 <n.rabone72@aol.com>
2025-12-12 10:13:38 +0000

    CreateNews - 2

--- client/src/dashboard/pages/CreateNews.jsx
+++ client/src/dashboard/pages/CreateNews.jsx
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom'
+
 const CreateNews = () => {
   return (
-    <div className='bg-grey-light-extra rounded-md p-6'>
-      <div className='just-between'></div>
+    <div>
+      <div className='just-between mb-6 p-4'>
+        <h2 className='text-xl font-medium uppercase'>Add News</h2>
+        <Link className='hover:text-primary font-medium uppercase'>
+          View All
+        </Link>
+      </div>
     </div>
   )
 }
