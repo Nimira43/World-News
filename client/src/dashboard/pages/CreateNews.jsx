cb511f55b9f78e2dcb854294a4a3d45e8f4288f6
Nimira43 <n.rabone72@aol.com>
2025-12-29 09:48:13 +0000

    Copied form from AddWriter

--- client/src/dashboard/pages/CreateNews.jsx
+++ client/src/dashboard/pages/CreateNews.jsx
@@ -13,18 +13,87 @@ const CreateNews = () => {
       </div>
 
       <form>
-        <div>
-          <label
-            htmlFor='title'
-            className='block text-md font-medium text-grey-medium mb'
-          >
-            Title
-          </label>
-        </div>
-
-
-      </form>
-
+          <div className='grid grid-cols-2 gap-x-8 mb-3'>
+            <div className='flex flex-col gap-y-2'>
+              <label 
+                htmlFor='name' 
+                className='text-grey-dark uppercase text-md'
+              >
+                Name  
+              </label>
+              <input
+                id='name' 
+                type='text' 
+                name='name'
+                required
+                className='input-field' 
+              />
+            </div>
+            <div className='flex flex-col gap-y-2'>
+              <label 
+                htmlFor='name' 
+                className='text-grey-dark uppercase text-md'
+              >
+                Category  
+              </label>
+              <select
+                id='name' 
+                name='category'
+                required 
+                className='input-field text-left'
+              >
+                <option 
+                value=''>Select Status</option>
+                <option value='International'>International</option>
+                <option value='Sport'>Sport</option>
+                <option value='Business'>Business</option>
+                <option value='Technology'>Technology</option>
+                <option value='Health'>Health</option>
+                <option value='Entertainment'>Entertainment</option>
+                <option value='Travel'>Travel</option>
+              </select>
+            </div>
+          </div>
+          <div className='grid grid-cols-2 gap-x-8 mb-3'>
+            <div className='flex flex-col gap-y-2'>
+              <label 
+                htmlFor='email' 
+                className='text-grey-dark uppercase text-md'
+              >
+                Email  
+              </label>
+              <input
+                id='email' 
+                type='email' 
+                name='email'
+                required
+                className='input-field' 
+              />
+            </div>
+            <div className='flex flex-col gap-y-2'>
+              <label 
+                htmlFor='password' 
+                className='text-grey-dark uppercase text-md'
+              >
+                Password  
+              </label>
+              <input
+                id='password' 
+                type='password' 
+                name='password'
+                required
+                className='input-field' 
+              />
+            </div>
+          </div>
+          <div className='mt-4'>
+            <button
+              className='px-3 py-[6px] font-medium bg-grey-dark hover:bg-primary text-light uppercase rounded-md'
+            >
+              Add Nres
+            </button>
+          </div>
+        </form>
 
     </div>
   )
