6f6f1f15daf0c26ff7c3ae8147be6f2cf8c8adc6
Nimira43 <n.rabone72@aol.com>
2026-03-18 05:59:41 +0000

    Created first model. The database is connecting and reading the model.

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,8 +1,14 @@
+const authModel = require('../models/authModel')
+
 class authController {
   login = async(req, res) => {
     console.log('This is the Controller.')
+    console.log('Controller hit.')
+    console.log('Body:', req.body)
+    res.send('ok')
+    const user = await authModel.findOne({ email: req.body.email });
+    console.log('User from DB:', user);
   }
-
 }
 
 module.exports = new authController()
