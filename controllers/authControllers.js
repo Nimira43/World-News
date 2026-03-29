885e77a4cc50fa3659e012f1f2322385939d5495
Nimira43 <n.rabone72@aol.com>
2026-03-29 08:36:24 +0100

    Setting up JWT token.

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,13 +1,45 @@
 const authModel = require('../models/authModel')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 class authController {
   login = async(req, res) => {
-    console.log('This is the Controller.')
-    console.log('Controller hit.')
-    console.log('Body:', req.body)
-    res.send('ok')
-    const user = await authModel.findOne({ email: req.body.email });
-    console.log('User from DB:', user);
+    const { email, password } = req.body
+
+    if (!email) {
+      return res.status(404).json({ message: 'Please provide your email.' })
+
+    }
+    if (!password) {
+      return res.status(404).json({ message: 'Please provide your password.' })
+    }
+
+    try {
+      const user = await authModel.findOne({ email: req.body.email }).select('+password')
+
+      if (user) {
+        const match = await bcrypt.compare(password, user.password)
+
+        if (match) {
+          const obj = {
+            id: user.id,
+            name: user.name,
+            category: user.category,
+            role: user.role
+          }
+          const token = await jwt.sign(obj, process.env.SECRET, {
+            expiresIn: process.env.EXPIRY_TIME
+          })
+          return res.status(200).json({message: 'Login succesful.', token})
+        } else {
+          return res.status(404).json({ message: 'Invalid password.'})
+        }
+      } else {
+        return res.status(404).json({ message: 'User not found.'})
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
 
