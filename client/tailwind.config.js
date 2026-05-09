53832fa72dcac54cecfe00718fbed96817fb48ee
Nimira43 <n.rabone72@aol.com>
2026-05-09 01:39:20 +0100

    Reviewing project.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -15,7 +15,13 @@ export default {
         'grey-light-extra': '#eee',
         'grey-light': '#dedede',
         'grey-medium': '#888',
-        'grey-dark': '#333'
+        'grey-dark': '#333',
+        'grey-1': '#333',
+        'grey-2': '#999',
+        'grey-3': '#ccc',
+        'grey-4': '#dedede',
+        'grey-5': '#eee',
+        'grey-6': '#fafafa'
       },
     },
   },
